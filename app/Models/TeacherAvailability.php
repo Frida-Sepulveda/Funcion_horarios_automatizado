f29149f2e96cd7c1e2ba55a308aa58e59104e508
Frida Paulina Sepúlveda Becerra <203.sepulveda.becerra.frida@gmail.com>
2026-05-24 14:08:09 -0600

    Se agrego el uso de factory en los modelos

--- app/Models/TeacherAvailability.php
+++ app/Models/TeacherAvailability.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TeacherAvailability extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'teacher_id',
         'day',
