630717c23a189ee2d23bbbe41ba6eed48bf5c0d3
Frida Paulina Sepúlveda Becerra <203.sepulveda.becerra.frida@gmail.com>
2026-05-24 14:11:21 -0600

    Se agregó el uso de factory

--- app/Models/ScheduleBlock.php
+++ app/Models/ScheduleBlock.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ScheduleBlock extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'day',
         'start_time',
